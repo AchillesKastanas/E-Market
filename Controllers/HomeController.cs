a6e53a641deced78b573bc61fb94e5bfebeb14e9
Anthony_Kalampogias <antonykalampogias@gmail.com>
2021-05-26 20:56:55 +0300

    Feat: Added basic funcions

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using e_market.Models;
 
 namespace e_market.Controllers
 {
@@ -14,18 +15,5 @@ namespace e_market.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
