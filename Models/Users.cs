29d95e7ee3ae67a322eb460fa478f6b1f69be8e7
Anthony_Kalampogias <antonykalampogias@gmail.com>
2021-05-31 20:17:55 +0300

    Feat: work on model

--- Models/Users.cs
+++ Models/Users.cs
@@ -7,6 +7,13 @@ namespace emarket.Models
 {
     public class Users
     {
-
+        public string Firstname { get; set; }
+        public string LastName { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsVendor { get; set; }
+        public string LastLogin { get; set; }
     }
 }
