00d43411c10353c6a0426e0c4ade98edc3fdf7df
GeorgeLiaros <47497244+GeorgeLiaros@users.noreply.github.com>
2021-06-04 20:20:41 +0300

    Feat: Work on project

---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace emarket.Models
+{
+    public class Product
+    {
+        public string Info { get; set; }
+        public string Price { get; set; }
+        public string AvailableQuantity { get; set; }
+    }
+}
